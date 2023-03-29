ebf5fd580db870d20e416be661d53fe1f656f2f1
Erik Erlandsson <erik.erlandsson@knowit.se>
2023-03-29 11:12:57 +0200

    red lowercase

---
+++ test/test_validator.py
@@ -0,0 +1,22 @@
+import pytest
+
+'''
+At least 1 lowercase character
+At least 1 uppercase character
+At least 1 number
+At least 1 underscore, dash, comma, or slash
+Password must be at least 6 characters long
+Max 12 characters long
+
+Abc-123 -> 
+
+
+a -> true
+A -> true
+1 -> true
+
+'''
+
+
+def test_lowercase_character():
+    assert validate('a') == True
