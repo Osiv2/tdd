3c52ade938f15c8a3cee2981365867ccc167b18f
Erik Erlandsson <erik.erlandsson@knowit.se>
2023-03-29 11:49:03 +0200

    green

--- src/validator.py
+++ src/validator.py
@@ -1,13 +1,16 @@
 def validate(password):
     contains_lower_case = False
     contains_upper_case = False
+    contains_digit = False
 
     for c in password:
         if c.islower():
             contains_lower_case = True
         if c.isupper():
             contains_upper_case = True
+        if c.isdigit():
+            contains_digit = True
 
-    if contains_lower_case and contains_upper_case:
+    if contains_lower_case and contains_upper_case and contains_digit:
         return True
     return False
