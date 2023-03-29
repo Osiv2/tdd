e5bcde797ed09382145904a54292f877a9c7c598
Erik Erlandsson <erik.erlandsson@knowit.se>
2023-03-29 15:22:46 +0200

    refactor

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,7 @@
 def parse(query):
     if query is None or query == "":
         return []
-    return [word.lower() for word in list(dict.fromkeys(query.split()))]
+
+    search_engine_query = list(dict.fromkeys(query.split()))
+
+    return [word.lower() for word in search_engine_query]
