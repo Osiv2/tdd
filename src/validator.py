d5a80ccfe3e27319f01102079fcfdfc439608e2a
Erik Erlandsson <erik.erlandsson@knowit.se>
2023-03-29 11:57:43 +0200

    green

--- src/validator.py
+++ src/validator.py
@@ -2,6 +2,7 @@ def validate(password):
     contains_lower_case = False
     contains_upper_case = False
     contains_digit = False
+    contains_dash = False
 
     for c in password:
         if c.islower():
@@ -10,9 +11,16 @@ def validate(password):
             contains_upper_case = True
         if c.isdigit():
             contains_digit = True
+        if isdash(c):
+            contains_dash = True
 
-    return is_valid_password(contains_lower_case, contains_upper_case, contains_digit)
+    return is_valid_password(contains_lower_case, contains_upper_case, contains_digit, contains_dash)
 
 
-def is_valid_password(contains_lower_case, contains_upper_case, contains_digit):
-    return contains_lower_case and contains_upper_case and contains_digit
+def is_valid_password(contains_lower_case, contains_upper_case, contains_digit, contains_dash):
+    return contains_lower_case and contains_upper_case and contains_digit and contains_dash
+
+
+def isdash(char):
+    if char == "-":
+        return True
