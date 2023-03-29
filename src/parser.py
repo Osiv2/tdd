d6b1e9021476fb184699800233c7a37f6745250f
Erik Erlandsson <erik.erlandsson@knowit.se>
2023-03-29 15:33:31 +0200

    green

--- src/parser.py
+++ src/parser.py
@@ -2,15 +2,15 @@ def parse(query):
     if query is None or query == "":
         return []
 
-    search_engine_query = list(dict.fromkeys(query.split()))
+    search_engine_query = [parse_word(word) for word in query.split()]
 
-    return [parse_word(word) for word in search_engine_query]
+    return list(dict.fromkeys(search_engine_query))
 
 
 def parse_word(word):
     word = word.lower()
     if word.endswith('s'):
         word = word[:-1]
-    if word == "coder":
+    if word in ["coder", "programmer"]:
         word = "developer"
     return word
