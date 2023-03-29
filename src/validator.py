9a7a52f72d3043b5aeef717054c445f330182d3e
Erik Erlandsson <erik.erlandsson@knowit.se>
2023-03-29 11:51:59 +0200

    refactor

--- src/validator.py
+++ src/validator.py
@@ -11,6 +11,8 @@ def validate(password):
         if c.isdigit():
             contains_digit = True
 
-    if contains_lower_case and contains_upper_case and contains_digit:
-        return True
-    return False
+    return is_valid_password(contains_lower_case, contains_upper_case, contains_digit)
+
+
+def is_valid_password(contains_lower_case, contains_upper_case, contains_digit):
+    return contains_lower_case and contains_upper_case and contains_digit
