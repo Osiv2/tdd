0b93f20a0dbdaa3cb897eef99b25e56d70b98d7e
Erik Erlandsson <erik.erlandsson@knowit.se>
2023-03-29 11:47:19 +0200

    refactor

--- src/validator.py
+++ src/validator.py
@@ -1,6 +1,13 @@
 def validate(password):
-    lowerCases = [c for c in password if c.islower()]
-    upperCases = [c for c in password if c.isupper()]
-    if len(lowerCases) and len(upperCases):
+    contains_lower_case = False
+    contains_upper_case = False
+
+    for c in password:
+        if c.islower():
+            contains_lower_case = True
+        if c.isupper():
+            contains_upper_case = True
+
+    if contains_lower_case and contains_upper_case:
         return True
     return False
